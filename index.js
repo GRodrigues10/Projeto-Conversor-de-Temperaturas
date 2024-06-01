9a5f25e4271c21e5c4c190248a4a96c36fd8f394
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-06-01 17:17:46 -0300

    Update

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+function calcular(){
+    const caixa = document.getElementById("caixa");
+    const cel = document.getElementById("grauC");
+    const fah = document.getElementById("grauF");
+    const res = document.getElementById("res");
+    let caixaNum = Number(caixa.value);
+    if(caixa.value.length == 0){
+        alert("Por favor, Insira um valor!");
+    }
+    else if(cel.checked){
+       let cels = (caixaNum - 32) * (5/9);
+       res.innerHTML = `Fahrenheit ➡ Celsius: ${cels.toFixed(1)}ºC `
+    }
+
+    else if(fah.checked){
+        let fah = (caixaNum * 9 / 5) + 32;
+        res.innerHTML = `Celsius ➡ Fahrenheit: ${fah.toFixed(1)}ºF `
+    }
+    else{
+        alert("Por favor, escolha uma opção!")
+    }
+   
+}
