7191e4323ab052244df611829f5f5ed237f334ff
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-06-01 18:47:14 -0300

    Atualização

--- index.js
+++ index.js
@@ -4,21 +4,41 @@ function calcular(){
     const fah = document.getElementById("grauF");
     const res = document.getElementById("res");
     let caixaNum = Number(caixa.value);
+    
     if(caixa.value.length == 0){
         alert("Por favor, Insira um valor!");
-    }
-    else if(cel.checked){
+    } else if(cel.checked){
        let cels = (caixaNum - 32) * (5/9);
-       res.innerHTML = `Fahrenheit ➡ Celsius: ${cels.toFixed(1)}ºC `
+       console.log("Temperatura em Celsius:", cels);
+       res.innerHTML = `Fahrenheit ➡ Celsius: ${cels.toFixed(1)}ºC `;
        
-    }
+        if (cels < 0) {
+            document.body.style.backgroundImage = 'url(imagens/neve.jpg)'; 
+        }
+        else if(cels > 0 && cels < 25){
+            document.body.style.backgroundImage = 'url(imagens/normal.jpg)'; 
+        }
 
-    else if(fah.checked){
+        else{
+            document.body.style.backgroundImage = 'url(imagens/calor.jpg)'; 
+        }
+       
+    } else if(fah.checked){
         let fah = (caixaNum * 9 / 5) + 32;
-        res.innerHTML = `Celsius ➡ Fahrenheit: ${fah.toFixed(1)}ºF `
+        console.log("Temperatura em Fahrenheit:", fah);
+        res.innerHTML = `Celsius ➡ Fahrenheit: ${fah.toFixed(1)}ºF `;
+
+        if (fah < 0) {
+            document.body.style.backgroundImage = 'url(imagens/neve.jpg)'; 
+        }
+        else if(fah > 0 && fah < 25){
+            document.body.style.backgroundImage = 'url(imagens/normal.jpg)'; 
+        }
+
+        else{
+            document.body.style.backgroundImage = 'url(imagens/calor.jpg)'; 
+        }
+    } else {
+        alert("Por favor, escolha uma opção!");
     }
-    else{
-        alert("Por favor, escolha uma opção!")
-    }
-   
 }
